68ac5016b3ed745eb47803c608828c6b4ad151b9
Teddy <teddykipchumba17@gmail.com>
2024-06-13 14:58:14 +0300

    added functions

--- lib/helpers.py
+++ lib/helpers.py
@@ -4,8 +4,12 @@ from models.supplier import Supplier
 
 def list_items():
     items = Item.get_all()
-    for item in items:
-        print(item)
+    if items:
+        for item in items:
+            if item:
+                print(item)
+            else:
+                print("Invalid item reference found and skipped.")
 
 def exit_program():
     print("Exiting program")
@@ -15,6 +19,15 @@ def find_item_by_id():
     id_ = input("Enter the item's id: ")
     item = Item.find_by_id(id_)
     print(item) if item else print(f'Item {id_} not found')
+
+def find_item_by_name():
+    name = input("Enter the item's name: ")
+    items = Item.find_by_name(name)
+    if items:
+        for item in items:
+            print(f"- {item.name}: Quantity - {item.quantity}, Price - ${item.price}")
+    else:
+        print("No items found matching the keyword.")
 
 def create_item():
     name = input("Enter the item's name: ")
@@ -27,21 +40,6 @@ def create_item():
         print(f'Success: {item}')
     except Exception as exc:
         print("Error creating item: ", exc)
-
-def create_category():
-    name = input("Enter the category name: ")
-    description = input("Enter the category description: ")
-    try:
-        category = Category.create(name, description)
-        print(f'Success: Category {category.name} created')
-    except Exception as exc:
-        print("Error creating category: ", exc)
-
-def list_categories():
-    categories = Category.get_all()
-    print("Categories:")
-    for category in categories:
-        print(f"- {category.name}: {category.description}")
 
 def update_item():
     id_ = input("Enter the item's id: ")
@@ -64,14 +62,13 @@ def update_item():
     else:
         print(f'Item {id_} not found')
 
-def filter_items_by_name():
-    keyword = input("Enter item name: ")
-    items = Item.find_by_name(keyword)
-    if items:
-        for item in items:
-            print(f"- {item.name}: Quantity - {item.quantity}, Price - ${item.price}")
+def delete_item():
+    id_ = input("Enter the item's id: ")
+    if item := Item.find_by_id(id_):
+        Item.delete(item.id)
+        print(f'Item {id_} deleted')
     else:
-        print("No items found matching the keyword.")
+        print(f'Item {id_} not found')
 
 def filter_items_by_category_or_supplier():
     choice = input("Filter by (C)ategory or (S)upplier: ").lower()
@@ -120,13 +117,71 @@ def filter_items_by_supplier():
     else:
         print("Invalid supplier ID or name.")
 
+def list_categories():
+    categories = Category.get_all()
+    print("Categories:")
+    for category in categories:
+        print(f"- {category.name}: {category.description}")
 
-def delete_item():
-    id_ = input("Enter the item's id: ")
-    if item := Item.find_by_id(id_):
-        Item.delete(item.id)
-        print(f'Item {id_} deleted')
+def create_category():
+    name = input("Enter the category name: ")
+    description = input("Enter the category description: ")
+    try:
+        category = Category.create(name, description)
+        print(f'Success: Category {category.name} created')
+    except Exception as exc:
+        print("Error creating category: ", exc)
+
+def delete_category():
+    id_ = input("Enter the category's id: ")
+    if category := Category.find_by_id(id_):
+        Category.delete(category.id)
+        print(f'Category {id_} deleted')
     else:
-        print(f'Item {id_} not found')
+        print(f'Category {id_} not found')
+
+def list_suppliers():
+    suppliers = Supplier.get_all()
+    print("Suppliers:")
+    for supplier in suppliers:
+        print(f"- {supplier.name}")
 
 
+
+def create_supplier():
+    name = input("Enter the supplier name: ")
+    contact = input("Enter the supplier contact: ")
+    address = input("Enter the supplier address: ")
+    try:
+        supplier = Supplier.create(name, contact, address)
+        print(f'Success: Supplier {supplier.name} created')
+    except Exception as exc:
+        print("Error creating supplier: ", exc)
+
+
+
+def update_supplier():
+    id_ = input("Enter the supplier's id: ")
+    if supplier := Supplier.find_by_id(id_):
+        try:
+            name = input("Enter the supplier's new name: ")
+            supplier.name = name
+            contact = input("Enter the supplier's new contact: ")
+            supplier.contact = contact
+            address = input("Enter the supplier's new address: ")
+            supplier.address = address
+            supplier.update()
+            print(f'Success: {supplier}')
+        except Exception as exc:
+            print("Error updating supplier: ", exc)
+    else:
+        print(f'Supplier {id_} not found')
+
+
+def delete_supplier():
+    id_ = input("Enter the supplier's id: ")
+    if supplier := Supplier.find_by_id(id_):
+        Supplier.delete(supplier.id)
+        print(f'Supplier {id_} deleted')
+    else:
+        print(f'Supplier {id_} not found')
